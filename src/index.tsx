3b60ae4c5759becfd7357a6438bfffece7b56bef
Tomasz Krzyżowski <tomasz.krzyzowski@cluify.com>
2019-01-30 23:47:09 +0100

    Preparation of the folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-// If you want your app to work offline and load faster, you can change
+ReactDOM.render(<App />, document.getElementById("root"));
+
+// if you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+// learn more about service workers: http://bit.ly/CRA-PWA
 serviceWorker.unregister();
